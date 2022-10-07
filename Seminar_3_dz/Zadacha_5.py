f4a3ad556b214a0494ecc05c4786d9288cd6bbde
Slava <migrant405@gmail.com>
2022-10-07 15:07:27 +0400

    ZAdacha_5

---
+++ Seminar_3_dz/Zadacha_5.py
@@ -0,0 +1,22 @@
+# Задайте число. Составьте список чисел Фибоначчи, 
+# в том числе для отрицательных индексов.
+
+
+
+
+# Не готова. пока не разобрался с отриццательными . 
+
+
+
+
+# num_first = 0
+# num_second = 1
+# num = (int(input("ВВедите число: ")))
+# for i in range(1,num):
+#     temp = num_first
+#     num_first = num_second
+#     num_second = num_first + temp
+#     print(num_first)
+
+
+
